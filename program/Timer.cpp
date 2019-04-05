739c2bc5987394cc111091118e8e4872c63f6a81
Louis Forbes Wright <louis.forbes9@gmail.com>
2019-04-05 14:55:47 +0100

    Timer C++ file

---
+++ program/Timer.cpp
@@ -0,0 +1,34 @@
+#include <time.h>
+#include <stdio.h>
+#include <csignal>
+#include <iostream>
+
+void signalHandler(int signum) {
+  printf("\ninterrupt signal %d received\a", signum);
+
+  exit(signum);
+}
+
+int main() {
+
+  signal(SIGINT, signalHandler);
+  clock_t start;
+
+  int waitingTime;
+
+  printf("how many minutes will you waiting?   \n");
+  scanf("%d",&waitingTime);
+
+  start = clock(); //record start time here
+  waitingTime = waitingTime * CLOCKS_PER_SEC * 60;
+  //calculates the number of cycles needed until required time is elapsed
+  printf("start waiting.. \n\n");
+
+  while(clock() - start < waitingTime + 1) {
+    printf("\r %d seconds have passed.", (clock()-start)/(CLOCKS_PER_SEC));
+}
+  //waits till required time has passed
+  printf("\n\afinished. \a");
+
+return 0;
+}
